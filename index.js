1475948cdf8989dc42f9038e30993ef20016857d
yetrun <89437145+yetrun@users.noreply.github.com>
2021-09-22 16:42:22 +0800

    添加跨域中间件

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 const port = 3000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
