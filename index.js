5b49baa134def0bdd0560c452ce2d971ff4c6970
yetrun <89437145+yetrun@users.noreply.github.com>
2021-11-08 17:22:23 +0800

    添加请求日志

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const restResource = require('./src/rest-resource')
+const morgan = require('morgan')
 
 const app = express()
 const port = process.argv.length > 2 ? parseInt(process.argv[2]) : 3000
 
+app.use(morgan('tiny'))
 app.use(cors())
 app.use(bodyParser.json())
 
